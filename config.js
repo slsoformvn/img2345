867263a2f15aaeb89f5ee061ae0840396ec61244
slsoformvn <85289441+slsoformvn@users.noreply.github.com>
2022-11-19 12:21:04 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-11';
+const titleCard = 'Tặng Thầy Cô';
+const contentCard = ' Nhân ngày 20/11,em xin chúc các thầy các cô lời chúc tốt đẹp nhất. Chúc các thầy cô hoàn thành nhiệm vụ cao cả của mình trong sự nghiệp trăm năm trồng người, trong công cuộc đổi mới của đất nước. Em chân thành cám ơn các thầy cô.';
+
+const giftImage = 'https://lh3.googleusercontent.com/pw/AL9nZEUYZMrQv74tdyIq2tPDtFIbigcnvJE0BtZIhrj8POBWVxHHehFUhNUfnJIhww_JR3d1p1EW4Q9OSq78UyK1U4G-dfp8mrx0L2mzD8HAO1wdW-lYEZmW877CtL1iMK8bY00YB-m00XlOIUOSrG3wQk82cqpiHbm5BCXvW-Y8IgjzkDftTFaj-Yyq2fVmR4NGZnea_6Nw_PGvXiC9S9JNmTHz4REXCf74q3uoefLlz-t_9IL7YFVqyRwFTN_ngZ64RFMJlIZ1CTVZWcdNc2FhYaEX2RNMbNFeI0FkA19Y1Q-XYw-BQWOuzqgqnDx2aTY8lQhfyiAygHpEu8WFxVPKTc4Ujs9nI2nVIhk9RASwGJqCjy4thlAaCToBVqu3wFj5eWpRKIfs9npRJHx_KRM6WDz4pYBCE_uqY6U0GfnHzbZRPpBA65dTS-cse11A9pGApdejIHk0bu2wmKFED-CFgrUOOTGk8nBiW-h6UEmw-Q1u8z2jdbkTdD7m0FWDUUp2uEq9AatQClYt92qVdAS-ny4JIu3umHZjyW_PftScLXdvJmrgxa6j2plU3bV6OpViYus2Xc9pW93kLrizR62OSFeKVebDGA9HONkAI8Jv_tRtb7hw9thFxbbT-ECB5A8hQ0hgpPwWaaw5CUClYB75qTTrVyhW-PdBE8Ji7Uq1P_eyjsadrYr-7TI9LtwZFEBjPgS5Y7pQyb9pqWggc_KigMmKy-C6_mAhfxMSNgcqreZM-SiO1FtjANJm3f_29ghYfa0yb9s6DZDc-A3XP3pyO07oshXh_Zhdf4HKLlMrqamSSUiba8auo3uSXYxcwN9bStgz2oWCMkgxrt9Bs3UrrHeBojo2QMJGKN-F9WHxbu43jZLsR3HHHOLsdAqMAX67Pz4dgaDjvPtMc1bbTlFCgMVLYFabWuwwN1ste_LBkBSw4-MCBsYbqVWRIdIHZVMA7L2cFP4agm_YvPt-U_A5H9DBELhEgQ=w401-h220-no?authuser=0';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
